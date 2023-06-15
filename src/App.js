045b4c058d624507a34ea03924d326f8dc4f3da0
Raiferdy541 <raihan.ferdyanza@gmail.com>
2023-06-15 19:52:10 +0700

    menambah tag nama dan id

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => {
     <>
       <div className='navbar'>
         <div className='studentInfo'>
-          {/* TODO: answer here */}
+        <h2 className="studentName">Raihan Ferdyanza</h2>
+        <p className="studentId">FE5235894</p>
         </div>
         <Link to='/'>Home</Link>
         <Link to='/photos'>My Photos</Link>
